e791aa7eb63e8e4425be5ed3a7be7f98bad6fcf9
Jacob Demoor <jake.demoor@gmail.com>
2017-11-25 15:03:35 -0500

    Playing around

--- index.php
+++ index.php
@@ -8,7 +8,35 @@
   <link rel="stylesheet" href="main.css">
 </head>
 <body>
-  hello world!
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Logic Reference</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#not">not</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#or">or</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#and">and</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#xor">xor</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  lets fuckin go borrrrrrrr
+
+  <div class="donkey" id="not">
+    yo yo im way down here
+  </div>
+
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
